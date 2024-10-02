bf68ff14b5890d081da261697cb92ee36d35cd25
El <105391840+El-404@users.noreply.github.com>
2024-10-01 20:08:54 -0500

    basic website

---
+++ README.md
@@ -0,0 +1,5 @@
+# The 9149 robotics website
+
+Visit the [9149 website](https://FRC9149.github.io)
+
+## To be Comepleted
---
+++ main.js
@@ -0,0 +1,7 @@
+//import { create3dObject } from "https://cdn.jsdelivr.net/gh/FRC9149/3d_JS_Library@main/9149_Rendering2.js";
+//
+//create3dObject(
+//  document.getElementById("container3d"),
+//  "/robot_models/file",
+//  spins = true
+//);
